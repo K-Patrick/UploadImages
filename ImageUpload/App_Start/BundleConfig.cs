b3cd32d9c6ead754511798a58f284a4ee14a2759
Kevin Patrick <kevinpa@helgesen.com>
2019-03-26 13:50:57 -0500

    Initial Release

--- ImageUpload/App_Start/BundleConfig.cs
+++ ImageUpload/App_Start/BundleConfig.cs
@@ -25,6 +25,9 @@ namespace ImageUpload
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+           
+
         }
     }
 }
--- ImageUpload/Controllers/HomeController.cs
+++ ImageUpload/Controllers/HomeController.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,18 +17,54 @@ namespace ImageUpload.Controllers
             return View();
         }
 
-        public ActionResult About()
+        [HttpPost]
+        public ActionResult UploadFiles(IEnumerable<HttpPostedFileBase> files)
         {
-            ViewBag.Message = "Your application description page.";
+            foreach (var file in files)
+            {
+                Image bmp = new Bitmap(file.InputStream);
+                Bitmap newImg = this.ResizeImage(bmp, 200, 200);
+                string filePath = Guid.NewGuid() + Path.GetExtension(file.FileName);
+                newImg.Save(Path.Combine(Server.MapPath("~/Images"), filePath));
+                //file.SaveAs(Path.Combine(Server.MapPath("~/Images"), filePath));
+                //here you can write code for save this information in your database if you want
+            }
 
-            return View();
+            return Json("file uploaded successfully");
         }
 
-        public ActionResult Contact()
+        /// <summary>
+        /// Resize the image to the specified width and height.
+        /// </summary>
+        /// <param name="image">The image to resize.</param>
+        /// <param name="width">The width to resize to.</param>
+        /// <param name="height">The height to resize to.</param>
+        /// <returns>The resized image.</returns>
+        public Bitmap ResizeImage(Image image, int width, int height)
         {
-            ViewBag.Message = "Your contact page.";
+            //http://www.itjungles.com/image-resize-maintain-aspect-ratio-formula.html
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
 
-            return View();
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
         }
+
     }
 }
