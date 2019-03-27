f4b7e274411a74a63363e9a4a6a1963d863767fe
Kevin Patrick <kevinpa@helgesen.com>
2019-03-27 07:04:23 -0500

    Upload and save images to file

--- ImageUpload/Controllers/HomeController.cs
+++ ImageUpload/Controllers/HomeController.cs
@@ -20,17 +20,20 @@ namespace ImageUpload.Controllers
         [HttpPost]
         public ActionResult UploadFiles(IEnumerable<HttpPostedFileBase> files)
         {
+            List<string> filenames = new List<string>();
             foreach (var file in files)
             {
                 Image bmp = new Bitmap(file.InputStream);
                 Bitmap newImg = this.ResizeImage(bmp, 200, 200);
                 string filePath = Guid.NewGuid() + Path.GetExtension(file.FileName);
-                newImg.Save(Path.Combine(Server.MapPath("~/Images"), filePath));
+                string saveFileName = Path.Combine(Server.MapPath("~/Images"), filePath);
+                newImg.Save(saveFileName);
+                filenames.Add("Images/" + filePath);
                 //file.SaveAs(Path.Combine(Server.MapPath("~/Images"), filePath));
                 //here you can write code for save this information in your database if you want
             }
 
-            return Json("file uploaded successfully");
+            return Json(filenames);
         }
 
         /// <summary>
@@ -43,11 +46,23 @@ namespace ImageUpload.Controllers
         public Bitmap ResizeImage(Image image, int width, int height)
         {
             //http://www.itjungles.com/image-resize-maintain-aspect-ratio-formula.html
+            
+            int newWidth = Convert.ToInt32((Double.Parse(image.Width.ToString()) / Double.Parse(image.Height.ToString())) * height);
+            int newHeight = Convert.ToInt32((Double.Parse(image.Height.ToString()) / Double.Parse(image.Width.ToString())) * width);
+           
+            if (newWidth <= width )
+            {
+                width = newWidth;
+            } else if (newHeight <= height )
+            {
+                height = newHeight;
+                
+            }
             var destRect = new Rectangle(0, 0, width, height);
             var destImage = new Bitmap(width, height);
 
             destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
-
+            
             using (var graphics = Graphics.FromImage(destImage))
             {
                 graphics.CompositingMode = CompositingMode.SourceCopy;
@@ -55,10 +70,11 @@ namespace ImageUpload.Controllers
                 graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
                 graphics.SmoothingMode = SmoothingMode.HighQuality;
                 graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
-
+                
                 using (var wrapMode = new ImageAttributes())
                 {
-                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    wrapMode.SetWrapMode(WrapMode.Clamp);
+                    
                     graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
                 }
             }
