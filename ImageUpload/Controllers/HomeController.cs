021d8358c41de69e6cab18f57a968d93f2d596f4
Kevin Patrick <kevinpa@helgesen.com>
2019-03-27 09:38:30 -0500

    Added class for Photo Management

--- ImageUpload/Controllers/HomeController.cs
+++ ImageUpload/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ImageUpload.Models;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Drawing2D;
@@ -12,74 +13,38 @@ namespace ImageUpload.Controllers
 {
     public class HomeController : Controller
     {
+
         public ActionResult Index()
         {
+            string imageLoc = "~/Images";
+            Photos p = new Photos(imageLoc, Server.MapPath(imageLoc));
+
+            ViewBag.photos = p.lst;
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult DeleteImage(FormCollection form)
+        {
+            var filepath = form["filepath"];
+            Photos p = new Photos();
+            p.DeletePhoto(Server.MapPath("~/" + filepath));
+            
+            return Json(true, JsonRequestBehavior.AllowGet);
+
         }
 
         [HttpPost]
         public ActionResult UploadFiles(IEnumerable<HttpPostedFileBase> files)
         {
-            List<string> filenames = new List<string>();
+            Photos p = new Photos();
+
             foreach (var file in files)
             {
-                Image bmp = new Bitmap(file.InputStream);
-                Bitmap newImg = this.ResizeImage(bmp, 200, 200);
-                string filePath = Guid.NewGuid() + Path.GetExtension(file.FileName);
-                string saveFileName = Path.Combine(Server.MapPath("~/Images"), filePath);
-                newImg.Save(saveFileName);
-                filenames.Add("Images/" + filePath);
-                //file.SaveAs(Path.Combine(Server.MapPath("~/Images"), filePath));
-                //here you can write code for save this information in your database if you want
+                p.SavePhoto(file, Server.MapPath("~/Images"));
             }
 
-            return Json(filenames);
-        }
-
-        /// <summary>
-        /// Resize the image to the specified width and height.
-        /// </summary>
-        /// <param name="image">The image to resize.</param>
-        /// <param name="width">The width to resize to.</param>
-        /// <param name="height">The height to resize to.</param>
-        /// <returns>The resized image.</returns>
-        public Bitmap ResizeImage(Image image, int width, int height)
-        {
-            //http://www.itjungles.com/image-resize-maintain-aspect-ratio-formula.html
-            
-            int newWidth = Convert.ToInt32((Double.Parse(image.Width.ToString()) / Double.Parse(image.Height.ToString())) * height);
-            int newHeight = Convert.ToInt32((Double.Parse(image.Height.ToString()) / Double.Parse(image.Width.ToString())) * width);
-           
-            if (newWidth <= width )
-            {
-                width = newWidth;
-            } else if (newHeight <= height )
-            {
-                height = newHeight;
-                
-            }
-            var destRect = new Rectangle(0, 0, width, height);
-            var destImage = new Bitmap(width, height);
-
-            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
-            
-            using (var graphics = Graphics.FromImage(destImage))
-            {
-                graphics.CompositingMode = CompositingMode.SourceCopy;
-                graphics.CompositingQuality = CompositingQuality.HighQuality;
-                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
-                graphics.SmoothingMode = SmoothingMode.HighQuality;
-                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
-                
-                using (var wrapMode = new ImageAttributes())
-                {
-                    wrapMode.SetWrapMode(WrapMode.Clamp);
-                    
-                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
-                }
-            }
-
-            return destImage;
+            return Json(p.lst);
         }
 
     }
---
+++ ImageUpload/Models/Photos.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace ImageUpload.Models
+{
+    public class Photos
+    {
+        public List<string> lst = new List<string>();
+
+        public string PhotoLocation { get; set; }
+        private DirectoryInfo d;
+
+        public Photos() { }
+
+        //USED TO GET THE FILES FROM SPECIFIED DIRECTORY - lst WILL CONTAIN THE LIST
+        public Photos(string PhotoDirectory, string serverPath)
+        {
+            this.PhotoLocation = PhotoDirectory;
+            d = new DirectoryInfo(serverPath);
+
+            FileInfo[] files = d.GetFiles();
+
+            foreach (FileInfo f in files)
+            {
+                if (f.Name != "Thumbs.db")
+                {
+                    lst.Add(PhotoLocation.Replace("~", "") + "/" + f.Name);
+                }
+
+            }
+        }
+
+        public void DeletePhoto(string filePath)
+        {
+            FileInfo f = new FileInfo(filePath);
+            f.Delete();
+
+        }
+
+        public void SavePhoto(HttpPostedFileBase file, string serverPath)
+        {
+            //CREATE THE BITMAP
+            Image bmp = new Bitmap(file.InputStream);
+            //RESIZE THE IMAGE
+            Bitmap newImg = this.ResizeImage(bmp, 200, 200);
+            //GENERATE A UNIQUE FILE NAME
+            string filePath = Guid.NewGuid() + Path.GetExtension(file.FileName);
+            //GENERATE FILE PATH
+            string saveFileName = Path.Combine(serverPath, filePath);
+            //SAVE FILE ON NETWORK
+            newImg.Save(saveFileName);
+            //ADD FILE TO LIST
+            lst.Add("/Images/" + filePath);
+        }
+
+        private Bitmap ResizeImage(Image image, int width, int height)
+        {
+            int newWidth = Convert.ToInt32((Double.Parse(image.Width.ToString()) / Double.Parse(image.Height.ToString())) * height);
+            int newHeight = Convert.ToInt32((Double.Parse(image.Height.ToString()) / Double.Parse(image.Width.ToString())) * width);
+
+            if (newWidth <= width)
+            {
+                width = newWidth;
+            }
+            else if (newHeight <= height)
+            {
+                height = newHeight;
+
+            }
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.Clamp);
+
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
+        }
+
+    }
+}
